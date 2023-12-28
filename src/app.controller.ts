2ed9b5210b012618e5941cdd98701bac441098f0
Teeraphat Sittinantakul <dev.teeraphat@gmail.com>
2023-12-28 15:25:18 +0700

    ✨ feat | first setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
+  @Get('')
   getHello(): string {
-    return this.appService.getHello();
+    return 'Hello world';
+  }
+
+  @Get('/health')
+  health(): string {
+    return 'Health Check !!';
   }
 }
